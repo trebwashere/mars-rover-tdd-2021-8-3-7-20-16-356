8d7f583a9c5327cd2bd85863b60d1b79ba98dcbd
Robert Pascual <robertjr.pascual@gmail.com>
2021-08-05 17:40:54 +0800

    [STMarsRover][Robert] feat: Added implementation for setting xCoords under Instructions object

--- src/main/java/com/afs/tdd/Application.java
+++ src/main/java/com/afs/tdd/Application.java
@@ -1,4 +1,10 @@
 package com.afs.tdd;
 
 public class Application {
+    public static Instructions spliceInstructions(String instructionsStr) {
+        String[] splicedInstructions = instructionsStr.split(" ");
+        return new Instructions(){{
+            setxCoords(Integer.parseInt(splicedInstructions[0]));
+        }};
+    }
 }
