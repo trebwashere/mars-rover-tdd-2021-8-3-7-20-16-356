80ce4cffe2737df2bcfd88cb075e886c6e4ed310
Robert Pascual <robertjr.pascual@gmail.com>
2021-08-05 20:11:38 +0800

    [STMarsRover][Robert] feat: Improved DirectionsEnum to contain final direction for each rotation

--- src/main/java/com/afs/tdd/DirectionsEnum.java
+++ src/main/java/com/afs/tdd/DirectionsEnum.java
@@ -1,18 +1,26 @@
 package com.afs.tdd;
 
 public enum DirectionsEnum {
-    NORTH("N"),
-    SOUTH("S"),
-    EAST("E"),
-    WEST("W");
+    NORTH("N","W", "E"),
+    SOUTH("S", "E", "W"),
+    EAST("E", "N", "S"),
+    WEST("W", "S", "N");
 
-    DirectionsEnum(String directionInitial) {
+    DirectionsEnum(String directionInitial, String directionAfterLeft, String directionAfterRight) {
         this.directionInitial = directionInitial;
+        this.directionAfterLeft = directionAfterLeft;
+        this.directionAfterRight = directionAfterRight;
     }
 
     private final String directionInitial;
+    private final String directionAfterLeft;
+    private final String directionAfterRight;
 
-    public String getDirectionInitial() {
-        return directionInitial;
+    public String getDirectionAfterLeft() {
+        return directionAfterLeft;
+    }
+
+    public String getDirectionAfterRight() {
+        return directionAfterRight;
     }
 }
